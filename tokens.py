bce7edc5e9512a570e98cc224d812179df815d9c
pfreitasbarbosa <contato.pfmb@gmail.com>
2021-03-15 19:54:27 -0300

    adiciona documentação aos tokens de palavra-chave

--- tokens.py
+++ tokens.py
@@ -4,21 +4,101 @@
 # Guilherme Ormond Sampaio | 22.218.007-7
 
 tokens = {
-    # keywords
-    "dcl" : "keyword",              # def (declare)
-    "tof" : "keyword",              # boolean, true or false
-    "ifi" : "keyword",              # if
-    "els" : "keyword",              # else
-    "elf" : "keyword",              # elif
-    "whl" : "keyword",              # while
-    "for" : "keyword",              # for
-    "str" : "type",                 # str
-    "num" : "type",                 # numeric type (int, float, double)
-    "emp" : "keyword",              # None, nil, null, similares
-    "brk" : "keyword",              # break
-    "jmp" : "keyword",              # continue
-    "ret" : "keyword",              # return
-    "vec\<(num|str)\>" : "keyword", # vector "vec\<(num|str)\>"                       
+    """
+        Palavra-chave que representa o tipo de valor booleano
+        Utilização:
+            tof tst = tru;
+    """
+    "tof" : "keyword",
+
+    """
+        Palavra-chave que representa o início de um controle de fluxo do tipo if
+        Utilização:
+            if tst == fls {...
+    """
+    "ifi" : "keyword",
+    
+    """
+        Palavra-chave que representa o início de um controle de fluxo do tipo else
+        Utilização:
+            else {...
+    """
+    "els" : "keyword",
+
+    """
+        Palavra-chave que representa o início de um controle de fluxo do tipo else if
+        Utilização:
+            elf tst == tru {...
+    """
+    "elf" : "keyword",
+
+    """
+        Palavra-chave que representa o início de um laço do tipo while
+        Utilização:
+            whl tst == tru {...
+    """
+    "whl" : "keyword",
+
+    """
+        Palavra-chave que representa o início de um laço do tipo for
+        Utilização:
+            for tst = 0:10 {...
+    """
+    "for" : "keyword",
+
+    """
+        Palavra chave que representa o tipo string
+        Utilização:
+            str tst = "eu sou uma string";
+    """
+    "str" : "type",
+
+    """
+        Palavra-chave que representa o tipo primitivo numérico (int, float, double etc)
+        Utilização:
+            num tst = 3.1415;
+    """
+    "num" : "type",
+
+    """
+        Palavra-chave que representa o comando break, que encerra um laço de repetição
+        Utilização:
+            for tst = 0:10 {...
+                ...
+                brk;
+            }
+    """
+    "brk" : "keyword",
+
+    """
+        Palavra-chave que representa o comando continue, que salta para a próxima 
+        iteração em um laço de repetição
+        Utilização:
+            for tst = 0:10 {...
+                ...
+                jmp;
+            }
+    """
+    "jmp" : "keyword",
+
+    """
+        Palavra-chave que representa o comando return, utilizado para transmitir 
+        alguma informação como saída de uma função
+        Utilização:
+            {
+                ...
+                ret tru;
+            } 
+    """
+    "ret" : "keyword",
+
+    """
+        Palavra-chave que representa a declaração de um vetor
+        Utilização:
+            vec<num> tst = [1, 3.14, 22];
+    """
+    "vec\<(num|str)\>" : "keyword",
+                       
     # identifiers
     "[A-z]{3}" : "var",             # 3 caracteres max, entre A-z
     "[0-9]+(\.[0-9]+)?" : "num",
