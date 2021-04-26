e285df5f4546c30ea50e6fcae530bbc5293d2fa3
seleonel <leon_bellini@outlook.com>
2021-04-25 22:19:05 -0300

    Adicionado dicionário de tokens aceitos
    Cada não-terminal possui seus tokens. As listas servem de controle para a
    chamada do método =eat=

--- simple_parser/parser.py
+++ simple_parser/parser.py
@@ -1,18 +1,37 @@
 class Parser(list):
     def __init__(self, lexer):
+        self.lexer = lexer
         # mais facil tratar uma lista sem None nesse caso, a lista é única, não
         # uma matriz
-        lexer.flatten_token_list()
+        self.lexer.flatten_token_list()
         # get_next_token funciona a partir de um iterador sobre essa lista
         # "plana" (lembrando que, toda vez que get_next_token é chamado, o
         # iterador sofre uma iteracao)
-        self.current_token = lexer.get_next_token()
+        self.current_token = self.lexer.get_next_token()
+        # dicionario de tokens aceitos em cada nivel
+        self.token_aceitos = {
+            "decvar": ["type", "var", "operator", "eos"],
+            "literal": ["str", "num"],
+            "bool": ["tru", "fls"],
+            "matlab": ["eos", "num", "(" ")"],
+            "operador": ["+", "-", "/", "*", "^"],
+            "flux": ["ifi", "scope_init", "scope_end", "els", "elf", "(", ")"],
+            "expr": ["(", ")", "and", "orr"],
+            "opbool": ["==", ">", ">=", "<", "<="],
+            "rpt": ["whl", "for", "(", ")",
+                    "scope_init", "scope_end", "type", "operator", "var"]
+        }
 
     def error(self):
         raise Exception("deu merda")  # arrumar depois
 
     def eat(self, token_type):
-        pass
+        # token = (tipo, valor, escopo)
+        # verifica apenas o primeiro endereço
+        if self.current_token[0] == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.erro()  # TODO implementar a classe de erro
 
     """
         Isso aqui é o S dos não terminais
